c29e809e5d24facaa5f7e8a83e5a39474216e09f
KyahWill <knownbutnotloved@gmail.com>
2022-11-23 20:52:48 +0800

    added yarn modules and worked on pages/login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css:["~/assets/styles/main.css"],
+  css:['~/assets/styles/main.css'],
+  modules: [
+   '@nuxtjs/supabase',
+   '@nuxtjs/google-fonts',
+  ],
 })
--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,22 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const email = ref('')
+  const password = ref('')
+  
+  const client = useSupabaseClient()
+
+  const login = async () => {
+    const {user, error} = await client.auth.signIn({
+      email: email.value,
+      password: password.value,
+    }) 
+  }
+</script>
 
 <template>
-  <div>
-    Page: foo
-  </div>
+  <form>
+    <input type="text" v-model="email"/>  
+    <input type="password" v-model="password"/>
+  </form>
 </template>
 
 <style scoped></style>
