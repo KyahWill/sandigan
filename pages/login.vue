7ab3d384a834cac48714065a75a236b374ac1ba7
KyahWill <knownbutnotloved@gmail.com>
2022-11-25 12:24:39 +0800

    fixed login status

--- pages/login.vue
+++ pages/login.vue
@@ -4,19 +4,43 @@
   
   const client = useSupabaseClient()
 
-  const login = async () => {
-    const {user, error} = await client.auth.signIn({
+  const login = async (event:any) => {
+    event.preventDefault()
+    console.log(client.auth)
+    const {user, error} = await client.auth.signInWithPassword({
       email: email.value,
       password: password.value,
     }) 
+    console.log(user)
+    console.log(error)
   }
 </script>
 
 <template>
-  <form>
-    <input type="text" v-model="email"/>  
-    <input type="password" v-model="password"/>
+  <form id="login">
+    <section class="login_box">
+      <div class="title">
+        <h2>SANDIGAN</h2>
+      </div>
+      <input type="text" v-model="email"/>  
+      <br />
+      <input type="password" v-model="password"/>
+      <br />
+      <button @click="login"> </button>
+      
+    </section>
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+#login {
+  display:flex;
+  justify-content:center;
+}
+.title {
+  display:flex;
+  justify-content:center;
+
+}
+</style>
