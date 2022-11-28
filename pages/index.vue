aea489d9f3f9eb6b38a7b0c19b9cdf9c7688f69f
KyahWill <knownbutnotloved@gmail.com>
2022-11-28 10:46:20 +0800

    Added connection to NEO4j javascript connection

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,36 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import neo4j from 'neo4j-driver'
+  
+  //Remove the capability of the user to see the credentials because this is a security issue
+  //Todo in the future
+  const driver = neo4j.driver(
+    'neo4j+s://60318b06.databases.neo4j.io',
+    neo4j.auth.basic('neo4j','Cq-Of1FHfShywvyaq0RpAJaOmIHA6ZVPW9yB6UxxXs8')
+  )
+  const session = driver.session()
+  const personName = 'Alice'
+ 
+  onMounted( async() => {
+    try {
+      const result =await session.executeRead(tx => {
+        return tx.run(
+          `MATCH (p:Person)
+          RETURN p.name AS name
+          LIMIT 10`,
+        )
+      })
+      console.log(result)
+      const node = singleRecord.get(0)
+
+      console.log(node.properties.name)
+    } finally {
+      await session.close()
+    } 
+
+    // on application exit:
+    await driver.close() 
+  })
+</script>
 
 <template>
   <section class="homepage">
