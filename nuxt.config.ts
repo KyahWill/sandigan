73bd4525cdcb344b69852b050a87c614f5612ad3
KyahWill <knownbutnotloved@gmail.com>
2023-01-05 14:29:07 +0800

    removed the need for supabase since we're switching to firebase now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
   css: ["~/assets/styles/main.css"],
   app: {
     head: {
