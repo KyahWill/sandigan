40cf79d5a21ed0f691fe2caba7039158375b390a
KyahWill <knownbutnotloved@gmail.com>
2023-01-20 13:53:44 +0800

    added the final steps needed for SSL

--- composables/graphDriver.ts
+++ composables/graphDriver.ts
@@ -2,9 +2,8 @@ import { driver, auth } from 'neo4j-driver'
 
 export const useDriver = () => {
   const neo4jDriver = driver(
-    'bolt://db.sandigan.org:7687',
-    auth.basic('neo4j', 'sandigan_thesis'),
-    { trust: 'TRUST_ALL_CERTIFICATES' }
+    'bolt+s://db.sandigan.org:7687',
+    auth.basic('neo4j', 'sandigan_thesis')
   )
   console.log(neo4jDriver)
   return neo4jDriver
