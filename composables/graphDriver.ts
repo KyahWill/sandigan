99aa9769b40ff733496489c664a113dea4845fc9
KyahWill <knownbutnotloved@gmail.com>
2023-01-20 12:19:16 +0800

    added the domain name for sandigan

--- composables/graphDriver.ts
+++ composables/graphDriver.ts
@@ -2,7 +2,7 @@ import { driver, auth } from 'neo4j-driver'
 
 export const useDriver = () => {
   const neo4jDriver = driver(
-    'bolt://159.65.141.247:7687',
+    'bolt://db.sandigan.org:7687',
     auth.basic('neo4j', 'sandigan_thesis'),
     { trust: 'TRUST_ALL_CERTIFICATES' }
   )
