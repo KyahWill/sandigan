c3213def039b8f63be062eb969e16fe406696493
KyahWill <knownbutnotloved@gmail.com>
2023-01-20 12:03:48 +0800

    removed encryption

--- composables/graphDriver.ts
+++ composables/graphDriver.ts
@@ -3,7 +3,8 @@ import { driver, auth } from 'neo4j-driver'
 export const useDriver = () => {
   const neo4jDriver = driver(
     'bolt://159.65.141.247:7687',
-    auth.basic('neo4j', 'sandigan_thesis')
+    auth.basic('neo4j', 'sandigan_thesis'),
+    { encrypted: 'ENCRYPTION_OFF' }
   )
   console.log(neo4jDriver)
   return neo4jDriver
